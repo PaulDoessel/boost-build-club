e43e2a7e84fbd85895731a9fbe27d919495fc8d2
Nick Porcino <meshula@hotmail.com>
2016-04-30 23:25:50 -0700

    Fix batch file

--- build-win-shared.bat
+++ build-win-shared.bat
@@ -1,12 +1,12 @@
 
-bootstrap.bat
+call bootstrap.bat
 copy boost-build-club-project-config.jam project-config.jam
 
 echo
 echo Building boost DLLs for VS2010, x86
 echo
-b2 --toolset=msvc-10.0 --build-type=complete stage link=shared
+echo b2 --toolset=msvc-14.0 --build-type=complete stage link=shared
 echo
 echo Building boost DLLs for VS2010, x64
 echo
-b2 --toolset=msvc-10.0 --build-type=complete architecture=x86 address-model=64 stage link=shared
+b2 --toolset=msvc-14.0 --build-type=complete architecture=x86 address-model=64 stage link=shared
--- build-win-static.bat
+++ build-win-static.bat
@@ -1,12 +1,12 @@
 
-bootstrap.bat
+call bootstrap.bat
 copy boost-build-club-project-config.jam project-config.jam
 
 echo
 echo Building boost static libs for VS2010, x86
 echo
-b2 --toolset=msvc-10.0 --build-type=complete stage link=static
+echo b2 --toolset=msvc-14.0 --build-type=complete stage link=static
 echo
 echo Building boost static libs for VS2010, x64
 echo
-b2 --toolset=msvc-10.0 --build-type=complete architecture=x86 address-model=64 stage link=static
+b2 --toolset=msvc-14.0 --build-type=complete architecture=x86 address-model=64 stage link=static
