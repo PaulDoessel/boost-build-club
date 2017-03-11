d884e35bdcca5323141b72d655b4c4eef312cd40
Paul Doessel <github@microspiff.com>
2017-03-12 00:02:23 +1100

    Update build-linux.sh
    this config was overriding everything...

--- build-linux.sh
+++ build-linux.sh
@@ -4,7 +4,7 @@ echo \n\n\n
 echo Building boost for Linux
 echo \n\n\n
 
-cp boost-build-club-project-config.jam project-config.jam
+#cp boost-build-club-project-config.jam project-config.jam
 
 ./b2 -j16 --build-dir=build-linux     --stagedir=stage-linux  --toolset=gcc \
 link=static threading=multi stage
