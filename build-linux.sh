0c0bd893728f226c290481e6f58576b1a94b8343
Paul Doessel <github@microspiff.com>
2017-03-11 18:57:56 +1100

    Create build-linux.sh

---
+++ build-linux.sh
@@ -0,0 +1,15 @@
+./bootstrap.sh
+
+echo \n\n\n
+echo Building boost for Linux
+echo \n\n\n
+
+cp boost-build-club-project-config.jam project-config.jam
+
+./b2 -j16 --build-dir=build-linux     --stagedir=stage-linux   --toolset=gcc \
+cxxflags="-std=c++11 -stdlib=libc++ -arch i386 -arch x86_64" linkflags="-stdlib=libc++" \
+link=static threading=multi stage
+
+echo \n\n\n
+echo Finished building boost for Linux
+echo \n\n\n
