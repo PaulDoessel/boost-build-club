be9bab604ea8b50bf5f4ac621c850129034ced56
Paul Doessel <github@microspiff.com>
2017-03-11 22:51:39 +1100

    Update build-linux.sh
    Do we need toolset?

--- build-linux.sh
+++ build-linux.sh
@@ -6,7 +6,7 @@ echo \n\n\n
 
 cp boost-build-club-project-config.jam project-config.jam
 
-./b2 -j16 --build-dir=build-linux     --stagedir=stage-linux   --toolset=gcc \
+./b2 -j16 --build-dir=build-linux     --stagedir=stage-linux  \
 cxxflags="-std=c++11 -stdlib=libc++ -arch i386 -arch x86_64" linkflags="-stdlib=libc++" \
 link=static threading=multi stage
 
